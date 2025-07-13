b12663bb943ce60cff7e49b5715dfbca622551e4
soye <stardust6653@gmail.com>
2025-07-14 00:04:31 +0900

    style : 컨텐츠 중앙 정렬 되도록 flex 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import CardList from "@/components/pages/home/CardList";
+import useGetDummyJsonData from "@/hooks/useGetDummyJsonData";
+import { useState } from "react";
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <div></div>;
+  const [page, setPage] = useState(1);
+  const { data, loading } = useGetDummyJsonData(page);
+
+  console.log(data);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <main className={styles.main}>
+      <CardList data={data?.products} />
+    </main>
+  );
 }
