78ba0df1cd02a027de5110782feb33b305b19694
soye <stardust6653@gmail.com>
2025-07-14 01:38:22 +0900

    feat : home('/') 이 아니라면 Banner 렌더링 되지 않도록 함

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,14 +1,20 @@
+"use client";
+
 import styles from "./Header.module.css";
 import Banner from "@/components/common/Banner/Banner";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>
         <span>Artinus Shop</span>
         <span className={styles.dot} />
       </h1>
-      <Banner />
+      {isHome && <Banner />}
     </header>
   );
 };
