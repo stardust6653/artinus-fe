f5930c50c554c80aba372bac6a61a9967ef73ca7
soye <stardust6653@gmail.com>
2025-07-14 00:51:14 +0900

    feat : Card 컴포넌트 내 기본 정보 외 데이터도 추가

--- src/components/pages/home/Card/index.tsx
+++ src/components/pages/home/Card/index.tsx
@@ -1,6 +1,7 @@
 import { ProductType } from "@/types/product";
 import styles from "./Card.module.css";
 import Image from "next/image";
+import Badge from "@/components/common/Badge/Badge";
 
 interface CardProps {
   data: ProductType;
@@ -9,9 +10,19 @@ interface CardProps {
 const Card = ({ data }: CardProps) => {
   return (
     <li className={styles.card} key={data.id}>
-      <Image src={data.thumbnail} alt={data.title} width={241} height={241} />
-
-      <h3>{data.title}</h3>
+      <Image
+        src={data.thumbnail}
+        alt={data.title}
+        width={241}
+        height={241}
+        className={styles.thumbnail}
+      />
+      <div className={styles.badgeList}>
+        <Badge status={data?.availabilityStatus} />
+        <Badge status={data?.category} />
+      </div>
+      <span className={styles.brand}>{data.brand}</span>
+      <h3 className={styles.title}>{data.title}</h3>
       <p>${data.price.toLocaleString()}</p>
     </li>
   );
