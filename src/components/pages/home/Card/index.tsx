02c55c6f07449b87e36c61cc76778fb3e3ffd959
soye <stardust6653@gmail.com>
2025-07-14 01:33:27 +0900

    feat : 카드 컴포넌트 내 Link 컴포넌트 추가 및 global css 에서 a태그 기본 스타일 제거

--- src/components/pages/home/Card/index.tsx
+++ src/components/pages/home/Card/index.tsx
@@ -3,6 +3,7 @@ import styles from "./Card.module.css";
 import Image from "next/image";
 import Badge from "@/components/common/Badge/Badge";
 import { calculatePriceInfo } from "@/utils/price";
+import Link from "next/link";
 
 interface CardProps {
   data: ProductType;
@@ -14,40 +15,42 @@ const Card = ({ data }: CardProps) => {
 
   return (
     <li className={styles.card} key={data.id}>
-      <div className={styles.thumbnailContainer}>
-        <Image
-          src={data.thumbnail}
-          alt={data.title}
-          width={241}
-          height={241}
-          className={styles.thumbnail}
-        />
+      <Link href={`/products/${data.id}`}>
+        <div className={styles.thumbnailContainer}>
+          <Image
+            src={data.thumbnail}
+            alt={data.title}
+            width={241}
+            height={241}
+            className={styles.thumbnail}
+          />
 
-        {data?.availabilityStatus === "Out of Stock" && (
-          <div className={styles.soldOut}>
-            <p>Sold Out</p>
-          </div>
-        )}
-      </div>
-      <div className={styles.badgeList}>
-        <Badge status={data?.availabilityStatus} />
-        <Badge status={data?.category} />
-      </div>
-      <span className={styles.brand}>{data.brand}</span>
-      <h3 className={styles.title}>{data.title}</h3>
-      <div className={styles.priceList}>
-        <p className={styles.originalPrice}>
-          ${originalPrice.toLocaleString()}
-        </p>
-        <div className={styles.discountInfo}>
-          <p className={styles.discountPercentage}>
-            {discountPercentage.toFixed(0)}%
-          </p>
-          <p className={styles.currentPrice}>
-            ${currentPrice.toLocaleString()}
-          </p>
+          {data?.availabilityStatus === "Out of Stock" && (
+            <div className={styles.soldOut}>
+              <p>Sold Out</p>
+            </div>
+          )}
         </div>
-      </div>
+        <div className={styles.badgeList}>
+          <Badge status={data?.availabilityStatus} />
+          <Badge status={data?.category} />
+        </div>
+        <span className={styles.brand}>{data.brand}</span>
+        <h3 className={styles.title}>{data.title}</h3>
+        <div className={styles.priceList}>
+          <p className={styles.originalPrice}>
+            ${originalPrice.toLocaleString()}
+          </p>
+          <div className={styles.discountInfo}>
+            <p className={styles.discountPercentage}>
+              {discountPercentage.toFixed(0)}%
+            </p>
+            <p className={styles.currentPrice}>
+              ${currentPrice.toLocaleString()}
+            </p>
+          </div>
+        </div>
+      </Link>
     </li>
   );
 };
